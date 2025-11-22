1aa9f90a67a4e1198416dadbcf9c4aa1709a40b8
Renato Ibarra <sainttrenvto@gmail.com>
2025-11-22 00:46:57 -0300

    fix: mejorar configuracion MySQL para PythonAnywhere

--- tribu_backend/settings.py
+++ tribu_backend/settings.py
@@ -70,8 +70,14 @@ if os.getenv('DATABASE_URL'):
     DATABASES = {
         'default': dj_database_url.config(
             default=os.getenv('DATABASE_URL'),
-            conn_max_age=600
+            conn_max_age=600,
+            conn_health_checks=True,
         )
+    }
+    # Opciones adicionales para MySQL en PythonAnywhere
+    DATABASES['default']['OPTIONS'] = {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        'charset': 'utf8mb4',
     }
 else:
     DATABASES = {
